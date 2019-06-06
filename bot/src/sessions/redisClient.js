ba8d6cb856a08592b0f8e069f896110772c06500
Diamondra Rasoamanana <diamondraras@gmail.com>
2019-06-06 13:00:15 +0000

    Update redisClient.js

--- bot/src/sessions/redisClient.js
+++ bot/src/sessions/redisClient.js
@@ -4,7 +4,7 @@ import redis from 'redis'
 export default redis.createClient({
     "port": process.env.REDIS_PORT,
 
-    // "password":  process.env.redisPassword,
+    "password":  process.env.REDIS_PASSWORD || "",
 
     "host": process.env.REDIS_HOST
 })
